1d1afe2c9d2cb6f7dfc638ae9e9f12dbd36f6dd3
vanakamcityxpress-source <vanakamcityxpress@gmail.com>
2025-11-20 10:57:15 +0530

    json

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",           // or "/your-subpath/"
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+  base: "/", // or "/subpath/" if deploying to subfolder
 });
