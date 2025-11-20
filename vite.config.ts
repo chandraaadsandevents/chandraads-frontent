8421c2673420936574090314331b6a96d8e4f650
vanakamcityxpress-source <vanakamcityxpress@gmail.com>
2025-11-20 10:40:47 +0530

    vite and json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // <-- make sure this is correct
+  base: "/", // ⚠️ Must match your deployment URL
 });
